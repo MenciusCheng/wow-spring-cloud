51cae4bb5b5efd13e239f50c954c29a76890599e
程孟威 <chengmengwei@iplas.com.cn>
2019-01-09 10:48:14 +0800

    zuul

---
+++ service-zuul/readme.md
@@ -0,0 +1,17 @@
+## [路由和过滤器: Zuul](https://cloud.spring.io/spring-cloud-static/Finchley.SR2/single/spring-cloud.html#_router_and_filter_zuul)
+
+路由是微服务架构不可或缺的一部分。例如，`/` 可以映射到您的 Web 应用程序，`/api/users` 映射到用户服务，`/api/shop` 映射到商店服务。Zuul 是 Netflix 的基于 JVM 的路由器和服务器端负载均衡器。
+
+Netflix 使用 Zuul 进行以下操作：
+
+- Authentication
+- Insights
+- Stress Testing
+- Canary Testing
+- Dynamic Routing
+- Service Migration
+- Load Shedding
+- Security
+- Static Response handling
+- Active/Active traffic management
+
---
+++ service-zuul/src/main/java/com/marvel/servicezuul/ServiceZuulApplication.java
@@ -0,0 +1,22 @@
+package com.marvel.servicezuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by Marvel on 19/01/08.
+ */
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class ServiceZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceZuulApplication.class, args);
+    }
+
+}
